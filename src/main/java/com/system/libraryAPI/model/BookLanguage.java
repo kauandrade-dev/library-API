bfb0df69b4117f7ecb44ae98e0b79386b3d2bb07
kauandrade-dev <kauandradedev@gmail.com>
2026-06-21 06:27:13 -0400

    feat: add book language enum

---
+++ src/main/java/com/system/libraryAPI/model/BookLanguage.java
@@ -0,0 +1,12 @@
+package com.system.libraryAPI.model;
+
+public enum BookLanguage {
+
+    // todo add more languages
+    PORTUGUESE,
+    SPANISH,
+    ENGLISH,
+    RUSSIAN,
+    JAPANESE,
+    CHINESE
+}
