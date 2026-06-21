c3bf499eb4f54b60602fd9290098869f6f528c76
kauandrade-dev <kauandradedev@gmail.com>
2026-06-21 06:24:38 -0400

    feat: add user category enum

---
+++ src/main/java/com/system/libraryAPI/model/BookCategory.java
@@ -0,0 +1,13 @@
+package com.system.libraryAPI.model;
+
+public enum BookCategory {
+
+    // todo add more categories
+    TERROR,
+    HORROR,
+    SUSPENSE,
+    NOVEL,
+    LIGHT_NOVEL,
+    MANGA,
+    ADVENTURE
+}
